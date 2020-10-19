381e5bc48519ad935f090538d33571147707d267
rahmanmdmizanur <rahman.mdmizanur@aist.go.jp>
2020-10-19 11:10:22 +0900

    add lemma

--- nlp/src/main/java/com/sam/nlp/Pipeline.java
+++ nlp/src/main/java/com/sam/nlp/Pipeline.java
@@ -7,7 +7,7 @@ import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 public class Pipeline {
 
     private static Properties properties;
-    private static String propertiesName = "tokenize, ssplit, pos";
+    private static String propertiesName = "tokenize, ssplit, pos, lemma";
     private static StanfordCoreNLP stanfordCoreNLP;
 
     private Pipeline() {
